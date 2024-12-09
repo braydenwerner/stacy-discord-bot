ae7241a7b85e2a41059ce2521cf229097dc238f6
braydenwerner <braydenwerner0@gmail.com>
2024-12-08 22:13:58 -0800

    Try catch

--- src/index.ts
+++ src/index.ts
@@ -7,49 +7,54 @@ import { Player } from "discord-player";
 import { Client, Collection, GatewayIntentBits } from "discord.js";
 import express from "express"
 
-console.log("token: ", process.env.TOKEN)
-
 export type CustomClient = Client & {
   commands: Collection<string, any>;
 };
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-  ],
-}) as CustomClient;
-client.commands = new Collection();
 
-// const player = new Player(client, {
-//   ytdlOptions: {
-//     quality: "highestaudio",
-//     highWaterMark: 1 << 25,
-//   },
-// });
-const player = new Player(client);
-// await player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
+try {
+  console.log("token: ", process.env.TOKEN)
 
-(async () => {
-  try {
-    await player.extractors.loadDefault();
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildVoiceStates,
+    ],
+  }) as CustomClient;
+  client.commands = new Collection();
 
-    registerMusicPlayerListeners(player);
-    createEventHandler(client);
-    createCommandHandler(client);
+  // const player = new Player(client, {
+  //   ytdlOptions: {
+  //     quality: "highestaudio",
+  //     highWaterMark: 1 << 25,
+  //   },
+  // });
+  const player = new Player(client);
+  // await player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
 
-    client.login(process.env.TOKEN);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-})();
+  (async () => {
+    try {
+      await player.extractors.loadDefault();
 
-// Google Cloud Health Check
-const port = process.env.PORT || 8080;
-const app = express()
-app.listen(port, () => {
-  console.log('Listening on port: ', port);
-});
+      registerMusicPlayerListeners(player);
+      createEventHandler(client);
+      createCommandHandler(client);
 
+      client.login(process.env.TOKEN);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  })();
+
+  // Google Cloud Health Check
+  const port = process.env.PORT || 8080;
+  const app = express()
+  app.listen(port, () => {
+    console.log('Listening on port: ', port);
+  });
+
+} catch (err) {
+  console.error(err)
+}
